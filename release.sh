cbaa1620fb7e727e1735655a5e462cf85ccffbe3
MihailKashinsev <mihailkasincevv@gail.com>
2026-03-08 02:51:42 +0300

    fix: auto-update version in App.jsx

--- release.sh
+++ release.sh
@@ -31,13 +31,16 @@ npm version $VERSION --no-git-tag-version
 echo "Обновляю tauri.conf.json..."
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" src-tauri/tauri.conf.json
 
+echo "Обновляю App.jsx..."
+sed -i '' "s/const VERSION = \".*\"/const VERSION = \"$VERSION\"/" src/App.jsx
+
 echo "Обновляю Cargo.toml..."
 sed -i '' "s/^version = \".*\"/version = \"$VERSION\"/" src-tauri/Cargo.toml
 
 echo "$CHANGELOG" > .changelog
 
 echo "Коммит..."
-git add package.json src-tauri/tauri.conf.json src-tauri/Cargo.toml .changelog
+git add package.json src-tauri/tauri.conf.json src-tauri/Cargo.toml src/App.jsx .changelog
 git commit -m "chore: release v$VERSION"
 
 echo "Создаю тег v$VERSION..."
