19ccec28b40d8e7ee9ae3e1da6d03e58191d744b
MihailKashinsev <mihailkasincevv@gail.com>
2026-03-08 20:32:35 +0300

    fix: GigaChat via tauri-plugin-http (bypasses CORS + Sber SSL)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_app::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
