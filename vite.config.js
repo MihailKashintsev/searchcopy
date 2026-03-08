a7cebf1704e3518f58a2ecacf0311260acdc4a93
MihailKashinsev <mihailkasincevv@gail.com>
2026-03-08 03:14:01 +0300

    fix: base path for production build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  base: "./",
+  build: { outDir: "dist" },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
