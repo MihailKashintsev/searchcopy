233fd41c5a8962cd91d41ea2b5af6c05c79bd54f
MihailKashinsev <mihailkasincevv@gail.com>
2026-03-08 03:47:00 +0300

    fix: revert vite config, remove google fonts

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react";
 
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-  base: "./",
-  build: { outDir: "dist" },
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -26,7 +18,6 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
