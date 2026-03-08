f4fc3618c043ad3a2be8f7fb05d6bb12710eb5b5
MihailKashinsev <mihailkasincevv@gail.com>
2026-03-08 21:54:50 +0300

    fix: import commands explicitly for generate_handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
 mod giga;
 mod updater;
+use giga::giga_search;
+use updater::download_and_install;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -9,7 +11,7 @@ pub fn run() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
-        .invoke_handler(tauri::generate_handler![greet, get_version, giga::giga_search, updater::download_and_install])
+        .invoke_handler(tauri::generate_handler![greet, get_version, giga_search, download_and_install])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
